03b09f8779b49edd935e647565de69ff1b1b5a09
Pavan-0228 <pavanrasal4@gmail.com>
2024-11-23 16:04:57 +0530

    Add root route and enhance database initialization error handling

--- app.js
+++ app.js
@@ -5,16 +5,24 @@ import errorHandler from "./middlewares/errorHandler.js";
 import dotenv from "dotenv";
 dotenv.config();
 
-
 const app = express();
 app.use(express.json({ limit: "16kb" }));
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
 
 app.use("/", schoolRoutes);
 
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
     console.log(`Server is running on port ${PORT}`);
-    initializeDatabase();
+    try {
+        await initializeDatabase();
+        console.log("Database initialized successfully");
+    } catch (error) {
+        console.error("Failed to initialize the database:", error.message);
+    }
 });
